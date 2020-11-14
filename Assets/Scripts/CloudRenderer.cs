7a980513b6414fb65e3e01a70c5e8ef0f938943d
DylanMcbean <dylanmcbean175@gmailcom>
2020-11-14 01:22:19 +0000

    clouds
    finnished clouds

--- Assets/Scripts/CloudRenderer.cs
+++ Assets/Scripts/CloudRenderer.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class CloudRenderer : MonoBehaviour
 {
-    public Sprite[] clouds;
-    GameObject[] cloud_objects;
+    public GameObject cloudObject;
+    List<GameObject> cloud_objects;
+    private float cloud_spawnCount = 0;
     // Start is called before the first frame update
     void Start()
     {
+        cloud_objects = new List<GameObject>();
         GenNewCloud();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = cloud_objects.Count-1; i >= 0; i--)
+        {
+            if (cloud_objects[i] != null)
+            {
+                cloud_objects[i].transform.position = new Vector3(cloud_objects[i].transform.position.x - Time.deltaTime * cloud_objects[i].transform.localScale.x, cloud_objects[i].transform.position.y, cloud_objects[i].transform.position.z);
+                if (cloud_objects[i].transform.position.x < -29)
+                {
+                    Destroy(cloud_objects[i]);
+                    cloud_objects.Remove(cloud_objects[i]);
+                    continue;
+                }
+            }
+        }
+
+        cloud_spawnCount += Random.Range(0.0f, 2.0f);
+        if(cloud_spawnCount > 1000)
+        {
+            GenNewCloud();
+            cloud_spawnCount = 0;
+        }
     }
 
     void GenNewCloud()
     {
-        GameObject new_cloud = new GameObject();
-        new_cloud.AddComponent<SpriteRenderer>();
-        new_cloud.GetComponent<SpriteRenderer>().sprite = clouds[Random.Range(0, clouds.Length)];
-        new_cloud.transform.position = new Vector3(10,Random.Range(-4.0f,16.0f),0);
-        float scale = Random.Range(1.0f, 3.0f);
-        new_cloud.transform.localScale = new Vector3(scale,scale,scale);
-        new_cloud.GetComponent<SpriteRenderer>().sortingOrder = new int[]{1,3 }[(int)Random.Range(0,2)];
+        GameObject new_cloud = Instantiate(cloudObject, transform);
         new_cloud.transform.parent = gameObject.transform;
+        cloud_objects.Add(new_cloud);
     }
 }
