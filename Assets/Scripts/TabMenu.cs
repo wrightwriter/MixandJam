e78056b4e3b26dc121c9bae35735a62d95751f8e
wright <jeyko1@gmail.com>
2020-11-14 13:35:30 +0200

    sprites and tab menu

---
+++ Assets/Scripts/TabMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabMenu : MonoBehaviour
+{
+
+    Canvas m_canvas;
+    void Start()
+    {
+        m_canvas = GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        m_canvas.enabled = Input.GetKey("tab");
+    }
+}
